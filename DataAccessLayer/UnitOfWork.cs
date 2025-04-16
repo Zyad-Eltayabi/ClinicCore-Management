14416bea11dfe0eb103009b474ea65e02718e198
Zyad <ZyadEltayibi@gmail.com>
2025-04-16 23:08:27 +0200

    implement repository pattern and unit of work pattern
    introduced repository and unit of work patterns to separate data access logic and improve maintainability.

---
+++ DataAccessLayer/UnitOfWork.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DomainLayer.Interfaces;
+
+namespace DataAccessLayer
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public int SaveChanges()
+        {
+            return _context.SaveChanges();
+        }
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
