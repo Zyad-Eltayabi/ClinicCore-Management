c8962246963deec01aa82e30968df58862036b2d
Zyad <ZyadEltayibi@gmail.com>
2025-05-29 01:38:03 +0300

    prevent duplicate prescriptions for the same medical record.
    Added a custom validator to ensure that a given medical record ID does not already exist in the prescriptions table when creating a new prescription. This prevents unintended duplication and enforces data integrity.

--- BusinessLayer/Validations/CreateOrUpdatePrescriptionValidator.cs
+++ BusinessLayer/Validations/CreateOrUpdatePrescriptionValidator.cs
@@ -7,23 +7,43 @@ namespace BusinessLayer.Validations;
 public class CreateOrUpdatePrescriptionValidator : AbstractValidator<CreateOrUpdatePrescriptionDto>
 {
     private readonly IUnitOfWork _unitOfWork;
+
     public CreateOrUpdatePrescriptionValidator(IUnitOfWork unitOfWork)
     {
         _unitOfWork = unitOfWork;
         Include(new CompletePrescriptionValidator());
-        
+
         RuleFor(p => p.PrescriptionID)
             .GreaterThan(0)
             .When(p => p.PrescriptionID != 0)
             .WithMessage("Prescription ID must be greater than 0 when specified.");
-        
+
         RuleFor(p => p.MedicalRecordId)
             .MustAsync(async (id, cancellation) => await IsMedicalRecordExist(id))
             .WithMessage("Medical Record does not exist.");
+        
+        // Prevent duplicate prescriptions by validating that a medical record ID doesn't exist in prescriptions table
+        RuleFor(p => p)
+            .CustomAsync(async (prescription, context, cancellation) =>
+            {
+                if (prescription.PrescriptionID == 0)
+                {
+                    bool exists = await IsMedicalRecordIdExistInPrescriptionsTable(prescription.MedicalRecordId);
+                    
+                    if (exists)
+                        context.AddFailure("MedicalRecordId", 
+                            "Medical Record ID already exists in prescriptions table.");
+                }
+            });
     }
-    
+
     private async Task<bool> IsMedicalRecordExist(int medicalRecordId)
     {
         return await _unitOfWork.MedicalRecords.ExistsAsync(m => m.MedicalRecordID == medicalRecordId);
+    }
+
+    private async Task<bool> IsMedicalRecordIdExistInPrescriptionsTable(int medicalRecordId)
+    {
+        return await _unitOfWork.Prescriptions.ExistsAsync(p => p.MedicalRecordId == medicalRecordId);
     }
 }
